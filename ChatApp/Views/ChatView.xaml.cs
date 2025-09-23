fa9422b5510f76e6a95c745d34c6732d692088c8
gabeamv <gabrielvillanueva11155@gmail.com>
2025-09-23 01:53:54 -0700

    implemented if scroll wheel is close to bottom when there is a message, automatically scroll to bottom. implemented with multiple subscribing to events.

--- ChatApp/Views/ChatView.xaml.cs
+++ ChatApp/Views/ChatView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -17,14 +18,49 @@ using System.Windows.Shapes;
 
 namespace ChatApp.Views
 {
-    public partial class ChatView : UserControl, INotifyCollectionChanged
+    // Chat GPT usage, so heavy commenting.
+    public partial class ChatView : UserControl
     {
+        private INotifyCollectionChanged? _currentCollection;
+
         public ChatView()
         {
+            // Data context changes at runtime, so we need a method to execute by then to check if and when the data
+            // context changes to the viewmodel.
+            DataContextChanged += ContextChanged;
             InitializeComponent();
         }
 
-        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        private void ContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Unsubscribe from old collection when the context has changed.
+            if (_currentCollection != null)
+            {
+                _currentCollection.CollectionChanged -= MessagesChanged;
+                return;
+            }
 
+            // Check if the current data context is the chatviewmodel data context.
+            if (DataContext is ChatViewModel chatViewModel && chatViewModel.ServerMessages is INotifyCollectionChanged observable)
+            {
+                // Store the observable server messages as Inotifycollectionchanged. observable collection implements inotifycollectionchanged
+                _currentCollection = observable;
+                // subscribe to the observable server messages. when the collection changes execute the subscribed method.
+                _currentCollection.CollectionChanged += MessagesChanged;
+            }
+        }
+
+        private void MessagesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (ScrollViewer.ScrollableHeight == 0) return;
+            // If the action that was done on the messages was adding a message...
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                // Calculate the distance of the scroll wheel from the bottom.
+                double dist = ScrollViewer.ScrollableHeight - ScrollViewer.VerticalOffset;
+                // If scroll wheel 100 pixels from the bottom, scroll wheel goes to the bottom
+                if (dist <= 100) ScrollViewer.ScrollToBottom();
+            }
+        }
     }
 }
